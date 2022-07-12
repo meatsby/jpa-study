8245a56ff3e60430c72a5303d4754ef1982e7d47
meatsby <meatsby@gmail.com>
2022-07-12 20:39:29 +0900

    feat: study embedded

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,34 +1,48 @@
 package hellojpa;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 @Entity
 public class Member {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
     private String name;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "team_id")
-    private Team team;
+    @Embedded
+    private Period workPeriod;
+
+    @Embedded
+    private Address homeAddress;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city", column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street", column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipcode", column = @Column(name = "WORK_ZIPCODE"))
+    })
+    private Address workAddress;
 
     protected Member() {
     }
 
-    public Member(final Long id, final String name, final Team team) {
+    public Member(final Long id, final String name, final Period workPeriod, final Address homeAddress,
+                  final Address workAddress) {
         this.id = id;
         this.name = name;
-        this.team = team;
+        this.workPeriod = workPeriod;
+        this.homeAddress = homeAddress;
+        this.workAddress = workAddress;
     }
 
     public Long getId() {
@@ -39,7 +53,15 @@ public class Member {
         return name;
     }
 
-    public Team getTeam() {
-        return team;
+    public Period getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public Address getWorkAddress() {
+        return workAddress;
     }
 }
